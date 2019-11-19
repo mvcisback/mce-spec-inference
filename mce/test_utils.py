a426ebc0fce3804050809c27fbcaee7d6d2b9f3d
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-11-18 20:55:26 -0800

    Added test utils

---
+++ mce/test_utils.py
@@ -0,0 +1,25 @@
+import aiger_bv as BV
+
+from mce.utils import empirical_sat_prob
+from mce.test_scenarios import SPEC1, SPEC2, SPEC3 
+
+
+ACTION_SEQS = 4*[3*[{'a': (True,), 'c': (True,)}]] + [
+    3*[{'a': (True,), 'c': (False,)}],
+    [{'a': (True,), 'c': (False,)}] + 2*[{'a': (False,), 'c': (True,)}],
+]
+
+TRCS = [(None, actions) for actions in ACTION_SEQS]
+
+
+def ltl2monitor(spec):
+    return BV.aig2aigbv(spec.aig)
+
+
+def test_empirical_sat_prob_smoke():
+    specs = [SPEC1, SPEC2, SPEC3]
+    psats = [7/8, 1, ]
+
+    for spec in zip(specs, psats):
+        monitor = ltl2monitor(SPEC1)
+        empirical_sat_prob(monitor, TRCS)
