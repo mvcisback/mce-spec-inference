9b6e982a7ca65412d6852afb086d8444610ec2d7
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-03-29 00:44:22 -0700

    Add sat prob to nodes + entropy bumps

--- mce/policy3.py
+++ mce/policy3.py
@@ -1,45 +1,99 @@
+__all__ = ["Policy", "policy", "fit"]
+
 from functools import partial
-from typing import Optional
+from typing import Optional, Hashable
 
 import attr
 import funcy as fn
 import networkx as nx
 import numpy as np
 from scipy.special import logsumexp
+from scipy.optimize import brentq
 
 from mce.spec import ConcreteSpec
 from mce.nx import spec2graph
 
 
-def policy(spec: ConcreteSpec, coeff: Optional[float] = None) -> nx.DiGraph:
+@attr.s(frozen=True, auto_attribs=True)
+class Policy:
+    graph: nx.DiGraph
+    root: Hashable
+
+    @property
+    def psat(self) -> float:
+        return self.graph.nodes[self.root]['psat']        
+
+
+def policy(spec: ConcreteSpec, coeff: Optional[float] = None):
     reference_graph, root, sink = spec2graph(spec)
-    
+
+    @fn.cache(5)  # Cache results for 5 seconds.
     def ppolicy(coeff):
         graph = reference_graph.reverse(copy=True)
-
 
         # Iterate in reverse topological order (ignoring dummy sink).
         for node in fn.rest(nx.topological_sort(graph)):
             if isinstance(node.var, bool):
-                graph.nodes[node]['val'] = coeff*int(node.var)
+                graph.nodes[node]['val'] = coeff*int(node.var)                
+                graph.nodes[node]['psat'] = int(node.var)
                 continue
                 
-            children = [c for (c, _) in graph.in_edges(node)]
-            vals = np.array([graph.nodes[c]['val'] for c in children])
+            kids = [c for (c, _) in graph.in_edges(node)]
+            vals = np.array([graph.nodes[c]['val'] for c in kids])
+            psats = np.array([graph.nodes[c]['psat'] for c in kids])
+
+            skipped = np.array([
+                spec.order.skipped_decisions(node.level, c.level) for c in kids
+            ])
+
 
             if not node.decision:
-                probs = np.array([graph[c][node]['label'] for c in children])
+                probs = np.array([graph[c][node]['label'] for c in kids])
+
+                # Note: This is arguably a bug in the model.
+                probs *= 2**skipped  # Account for compressed paths.
+
                 graph.nodes[node]['val'] = (probs * vals).sum()
+                graph.nodes[node]['psat'] = (probs * psats).sum()
                 continue
 
-            graph.nodes[node]['val'] = logsumexp(vals)
-            for child in children:
-                delta = graph.nodes[child]['val'] - graph.nodes[node]['val']
-                graph[child][node]['label'] = np.exp(delta)
+            state_val = graph.nodes[node]['val'] = logsumexp(vals)
+            probs = []
+            for k, child in zip(skipped, kids):
+                action_val = graph.nodes[child]['val']
+
+                # Note: This is arguably a bug in the model.
+                action_val += k * np.log(2)  # Account for skipped paths.
+
+                prob_action = np.exp(action_val - state_val)
+                graph[child][node]['label'] = prob_action
+                probs.append(prob_action)
+
+            graph.nodes[node]['psat'] = (probs * psats).sum()
         
-        return graph.reverse(copy=False)
+        return Policy(graph.reverse(copy=False), root=root)
 
     if coeff is None:
         return ppolicy
 
     return ppolicy(coeff)
+
+
+def fit(cspec: ConcreteSpec, psat: float, top: float=100) -> Policy:
+    pctrl = policy(cspec)
+
+    def f(coeff):
+        return pctrl(coeff).psat - psat
+
+    if f(-top) > 0:
+        coeff = 0
+    elif f(top) < 0:
+        coeff = top
+    else:
+        coeff = brentq(f, -top, top)
+
+    if coeff < 0:
+        # More likely the negated spec than this one.
+        coeff = 0  
+
+    return pctrl(coeff)
--- mce/test_policy3.py
+++ mce/test_policy3.py
@@ -2,11 +2,12 @@ import pytest
 
 import aiger_bv as BV
 import aiger_coins as C
+import funcy as fn
 from networkx.drawing.nx_pydot import write_dot
 
 from mce.test_scenarios import scenario_reactive
 from mce.spec import concretize
-from mce.policy3 import policy
+from mce.policy3 import policy, fit
 
 
 def test_policy():
@@ -14,15 +15,28 @@ def test_policy():
     monitor = C.MDP(BV.aig2aigbv(spec.aig) | BV.sink(1, ['c_next']))
     cspec = concretize(monitor, sys, 3)
 
-    graph = policy(cspec, 3)
+    ctrls = [policy(cspec)(3), policy(cspec, 3)]
+    for i, ctrl in enumerate(ctrls):
+        assert 0 <= ctrl.psat <= 1
+        graph = ctrl.graph
+        assert len(graph.nodes) == 10
+        assert len(graph.edges) == 16
 
-    assert len(graph.nodes) == 10
-    assert len(graph.edges) == 16
+        for node in graph.nodes:
+            assert graph.out_degree[node] <= 2
 
-    for node in graph.nodes:
-        assert graph.out_degree[node] <= 2
-        
-        prob = sum(graph[x][y]['label'] for x, y in graph.out_edges(node))
-        assert pytest.approx(prob, 1)
+            prob = sum(graph[x][y]['label'] for x, y in graph.out_edges(node))
+            assert pytest.approx(prob, 1)
 
-    write_dot(graph, 'foo4.dot')
+    pctrl = policy(cspec)
+    # Agent gets monotonically more optimal
+    psats = [pctrl(x).psat for x in range(10)]
+    assert all(x >= y for x, y in fn.with_prev(psats, 0))
+
+
+def test_fit():
+    spec, sys = scenario_reactive()
+    monitor = C.MDP(BV.aig2aigbv(spec.aig) | BV.sink(1, ['c_next']))
+    cspec = concretize(monitor, sys, 3)
+
+    assert fit(cspec, 0.7).psat == pytest.approx(0.7)
